6bedbedb6fc965ef618909b0bdaaae7c5c1c174a
maddiewaldie <maddiewaldie@me.com>
2022-01-21 21:05:08 -0500

    fixed operator+= to be using copy

--- Lab-3/sequence1.cpp
+++ Lab-3/sequence1.cpp
@@ -197,12 +197,24 @@ namespace coen79_lab3
         assert((rhs.size()+total) < CAPACITY);
         
         // Go through the rhs array and add the elements to the existing (lhs) array
-        for(size_type i = 0; i < rhs.size(); i++)
-        {
+        //for(size_type i = 0; i < rhs.size(); i++)
+        //{
             // Increment total while also adding the rhs elements to the lhs elements
-            array[total++] = rhs.array[i];
-        }
+            //array[total++] = rhs.array[i];
+        //}
+
+        copy(rhs.array, rhs.array + rhs.total, array + total);
+        total += rhs.total;
     }
+
+    /* sequence operator+=(sequence& lhs, sequence& rhs) { // Add the items of rhs to the lhs
+        // First, we need to make sure the lhs and rhs's number of elements combined is less than the total capacity
+        assert((lhs.size() + rhs.size()) < sequence::CAPACITY);
+
+        // If so, we can proceed with adding the two arrays
+        copy(rhs.array, rhs.array + rhs.total, lhs.array + lhs.total);
+        lhs.total += rhs.size();
+    } */
 
     sequence::size_type sequence::size() const { // Return the total # of elements in the sequence
         return total;
