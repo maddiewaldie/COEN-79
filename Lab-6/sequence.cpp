3ff689df0c7d47c95cbb75234c1fb123824d7f41
maddiewaldie <maddiewaldie@me.com>
2022-02-10 13:46:08 -0500

    maybe fixed?

--- Lab-6/sequence.cpp
+++ Lab-6/sequence.cpp
@@ -44,137 +44,229 @@ namespace coen79_lab6
     }
 
     // CONSTRUCTORS and DESTRUCTOR
-
-    //CONSTRUCTOR IMPLEMENTATION for default constructor
-    sequence::sequence ()
+    sequence::sequence()
     {
         init();
     }
-
-    //Copy Constructor
     sequence::sequence(const sequence& source)
     {
         init();
         *this = source;
     }
-
-    sequence::~sequence( ) {
-        // Clear the list
+    sequence::~sequence()
+    {
         list_clear(head_ptr);
-
-        // Reset the variables (head_ptr, tail_ptr, cursor, and precursor to NULL and many_nodes to 0)
-        init();
+        many_nodes = 0;
     }
 
     // MODIFICATION MEMBER FUNCTIONS
-    void sequence::start( ) {
+    void sequence::start()
+//     Postcondition: The precursor and cursor are adjusted such that the first
+//     item in the sequence becomes the current item (but if the sequence is
+//     empty, then there is no current item).
+    {
+        precursor = head_ptr;
         cursor = head_ptr;
-        precursor = NULL;
     }
-
-    void sequence::end() {
-        precursor = list_locate(head_ptr, many_nodes - 1);
-        cursor = precursor->link();
+    void sequence::end()
+//     Postcondition: The precursor and cursor are adjusted such that the last
+//     item in the sequence becomes the current item (but if the sequence is
+//     empty, then there is no current item).
+    {
+        cursor = tail_ptr;
+        if(many_nodes == 0)
+            cursor = precursor = NULL;
+        if(head_ptr == tail_ptr)
+            precursor = NULL;
+        precursor = head_ptr;
+        while(precursor->link() != cursor)
+            precursor = precursor->link();
     }
-
-    void sequence::advance( ) {
+    void sequence::advance()
+//     Precondition: is_item returns true.
+//     Postcondition: If the current item was already the last item in the
+//     sequence, then there is no longer any current item. Otherwise, the new
+//     current item is the item immediately after the original current item.
+    {
+        //std::cout << "advance1" << endl;
         assert(is_item());
+        
+        if (cursor == tail_ptr)
+        {
+            cursor = NULL;
+            return;
+        }
+        
         precursor = cursor;
         cursor = cursor->link();
     }
 
-    void sequence::insert(const value_type& entry) {
-        if(cursor == NULL){
+    void sequence::insert(const value_type& entry)
+//     Postcondition: A new copy of entry has been inserted in the sequence
+//     before the current item. If there was no current item, then the new entry
+//     has been inserted at the front of the sequence. In either case, the newly
+//     inserted item is now the current item of the sequence.
+    {
+        if(head_ptr == NULL)
+        {
             list_head_insert(head_ptr, entry);
             cursor = head_ptr;
             precursor = NULL;
             tail_ptr = head_ptr;
+            many_nodes++;
         }
-        else if(precursor == NULL){
-            list_head_insert(head_ptr,entry);
+        else if(is_item() && cursor == head_ptr)
+        {
+            list_head_insert(head_ptr, entry);
             cursor = head_ptr;
             precursor = NULL;
+            many_nodes++;
         }
-        else{
+        else if(is_item() && cursor != head_ptr)
+        {
             list_insert(precursor, entry);
             cursor = precursor->link();
+            many_nodes++;
         }
-        many_nodes++;
-    }
-
-    void sequence::attach(const value_type& entry) {
-        if(head_ptr == NULL){
+        else if (!is_item())
+        {
             list_head_insert(head_ptr, entry);
+            tail_ptr = precursor->link();
             cursor = head_ptr;
             precursor = NULL;
+            many_nodes++;
+        }
+    }
+    void sequence::attach(const value_type& entry)
+//     Postcondition: A new copy of entry has been inserted in the sequence after
+//     the current item. If there was no current item, then the new entry has
+//     been attached to the end of the sequence. In either case, the newly
+//     inserted item is now the current item of the sequence.
+    {
+        if(head_ptr == NULL)
+        {
+            list_head_insert(head_ptr, entry);
+            precursor = NULL;
+            cursor = head_ptr;
             tail_ptr = head_ptr;
+            many_nodes++;
         }
-        else if(cursor == NULL){
-            list_insert(tail_ptr, entry);
-            cursor = tail_ptr->link();
-            precursor = tail_ptr;
-            tail_ptr = cursor;	
+        else if(is_item() && many_nodes == 1)
+        {
+            list_insert(head_ptr, entry);
+            precursor = head_ptr;
+            tail_ptr = precursor->link();
+            cursor = tail_ptr;
+            many_nodes++;
         }
-        else{
-            list_insert(cursor, entry);
-            precursor = cursor;
-            cursor = cursor->link();
-            if(cursor->link() == NULL)
-                tail_ptr = cursor;		
-        }
-        many_nodes++;
-    }
-
-    void sequence::operator =(const sequence& source) {
-        //clean up existing data
-        list_clear(head_ptr);
-        //copy over new object
-        if(source.cursor == NULL){
-            list_copy(source.head_ptr, head_ptr,tail_ptr);
-            cursor = NULL;
-            precursor = NULL;
-        }
-        else if(source.cursor == source.head_ptr){
-            list_copy(source.head_ptr, head_ptr, tail_ptr);
-            precursor = NULL;
-            cursor = head_ptr;
-        }	
-        else{
-            list_piece(source.head_ptr, source.precursor, head_ptr, precursor);
-            list_piece(source.cursor, source.tail_ptr, cursor, tail_ptr);
-            precursor->set_link(cursor);	
-        }
-        many_nodes = source.size();
-    }
-
-    void sequence::remove_current( ) {
-        assert(is_item());
-        if(precursor == NULL){
-            list_head_remove(cursor);
-            cursor = head_ptr;
-        }
-        else{
-            list_remove(precursor);
+        else if(is_item() && cursor == head_ptr)
+        {
+            list_insert(head_ptr, entry);
+            precursor = head_ptr;
             cursor = precursor->link();
-            if(precursor->link() == NULL)
-                precursor = NULL;
+            many_nodes++;
         }
-        many_nodes--;
+        else if(is_item() && cursor != head_ptr && cursor != tail_ptr)
+        {
+            list_insert(cursor, entry);
+            advance();
+            many_nodes++;
+        }
+        else if (is_item() && cursor != head_ptr)
+        {
+            list_insert(tail_ptr, entry);
+            tail_ptr = cursor->link();
+            precursor = cursor;
+            cursor = tail_ptr;
+            many_nodes++;
+        }
+        else //if(!is_item())
+        {
+            list_insert(tail_ptr, entry);
+            precursor = tail_ptr;
+            cursor = precursor->link();
+            tail_ptr = cursor;
+            many_nodes++;
+        }
     }
+    void sequence::operator =(const sequence& source)
+    {
+        list_clear(head_ptr);
+        many_nodes = 0;
+        node* tail;
+        list_copy(source.head_ptr, head_ptr,tail);
+        tail_ptr = tail;
+        cursor = head_ptr;
+        
+        node* head = source.head_ptr;
+        precursor = head_ptr;
     
+        if(source.cursor == NULL)
+        {
+            cursor = NULL;
+            precursor = tail_ptr;
+        }
+        else if(precursor != NULL)
+            cursor = precursor->link();
+        else
+        {
+            while(head != source.precursor)
+            {
+                head = head->link();
+                precursor = precursor->link();
+            }
+        }
+        many_nodes = source.many_nodes;
+    }
+
+    void sequence::remove_current()
+//     Precondition: is_item returns true.
+//     Postcondition: The current item has been removed from the sequence, and
+//     the item after this (if there is one) is now the new current item.
+    {
+        assert(is_item());
+        if(head_ptr->link() == NULL)
+        {
+            delete cursor;
+            head_ptr = tail_ptr = precursor = cursor = NULL;
+            many_nodes--;
+        }
+        else if(cursor == head_ptr && head_ptr ->link() !=NULL)
+        {
+            cursor = head_ptr->link();
+            list_head_remove(head_ptr);
+            precursor = NULL;
+            many_nodes--;
+        }
+        else if(cursor != tail_ptr && cursor != head_ptr)
+        {
+            precursor->set_link(cursor->link());
+            delete cursor;
+            cursor = precursor->link();
+            many_nodes--;
+        }
+        else if (cursor == tail_ptr && many_nodes > 1)
+        {
+            tail_ptr = precursor;
+            tail_ptr->set_link(NULL);
+            delete cursor;
+            cursor = NULL;
+            many_nodes--;
+        }
+    }
+
     // CONSTANT MEMBER FUNCTIONS
-    sequence::size_type sequence::size( ) const {
+    sequence::size_type sequence::size() const
+    {
         return many_nodes;
     }
-
-    bool sequence::is_item( ) const {
-        if(cursor == NULL)
-            return false;
-        return true;	
+    bool sequence::is_item() const
+    {
+        return (cursor != NULL);
     }
-
-    sequence::value_type sequence::current( ) const {
-        assert(is_item());	
+    sequence::value_type sequence::current() const
+    {
+        assert(is_item());
         return cursor->data();
     }
 
