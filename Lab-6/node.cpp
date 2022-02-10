cac2e1f4fe9d947574934b440b7a0ed84c78883f
maddiewaldie <maddiewaldie@me.com>
2022-02-10 00:07:40 -0500

    commented node.cpp

--- Lab-6/node.cpp
+++ Lab-6/node.cpp
@@ -150,150 +150,228 @@ namespace coen79_lab6
 
 	// NONMEMBER FUNCTIONS TO IMPLEMENT!
 	void list_piece(node* start_ptr, node* end_ptr, node*& head_ptr, node*& tail_ptr) {
+		// First, set the head and tail to be NULL
 		head_ptr = NULL;
 		tail_ptr = NULL;
 	
-		if(start_ptr == NULL || start_ptr == end_ptr)
-			return;
+		// If the linked list is empty or 1 element, we don't want to proceed
+		if(start_ptr == NULL || start_ptr == end_ptr) { 
+			return;	// So, we can return
+		}
 
-		
-		list_head_insert(head_ptr, start_ptr->data());
+		// Next, a node containing start_ptr->data() can be inserted after head_ptr
+		list_head_insert(head_ptr, start_ptr -> data());
+
+		// And, we can set the tail_ptr to the head_ptr
 		tail_ptr = head_ptr;
-									//copy the nodes and traverse the list
-		while(start_ptr->link() != NULL &&  start_ptr->link() != end_ptr){
-			list_insert(tail_ptr, start_ptr->link()->data());
-			tail_ptr = tail_ptr->link();
-			start_ptr = start_ptr->link();
+		
+		// Now, we just need to traverse the list and copy the nodes
+		while((start_ptr -> link() != NULL) &&  (start_ptr -> link() != end_ptr)) {
+			// Insert a node containing start_ptr -> link() -> data() after the current tail_ptr
+			list_insert(tail_ptr, start_ptr -> link() -> data());
+			// Move the tail_ptr over
+			tail_ptr = tail_ptr -> link();
+			// Move the start_ptr over
+			start_ptr = start_ptr -> link();
 		}
 	}
 
     size_t list_occurrences(node* head_ptr, const node::value_type& target) {
+		// First, set a counter to 0, which will keep track of the occurrences of the target
 		size_t count = 0;
-		while(head_ptr != NULL){				//traverse to end of list
-			if(head_ptr->data() == target)
+
+		// Go through the list
+		while(head_ptr != NULL) {
+			// If the current element in the linked list equals the target, we can increment our counter
+			if(head_ptr -> data() == target) {
 				count++;
-			head_ptr = head_ptr->link();
+			}
+
+			// Move to the next element in the list
+			head_ptr = head_ptr -> link();
 		}
+
+		// Return the number of occurrences of the target in the list
 		return count;
 	}
 
     void list_insert_at(node*& head_ptr, const node::value_type& entry, size_t position) {
-		assert(position > 0);
-		assert(position <= list_length(head_ptr)+1);
+		// First, we need to check the preconditions
+		assert((position > 0) && (position <= list_length(head_ptr) + 1));
 		
-		if(position == 1){
+		// If the position is only one, we just need to insert the entry at the head_ptr and then return
+		if(position == 1) {
 			list_head_insert(head_ptr, entry);
 			return;
 		}
 		
+		// Create a temporary variable to keep track of the node we're at
 		node* temp = head_ptr;
-		size_t pos_count = 1;
-								//iterate to position						
-		while(pos_count < position -1){
-			temp = temp->link();
-			pos_count++;
+		// Create a temporary variable to keep track of the position we're at
+		size_t positionNumber = 1;
+
+		// Loop through the list to the position					
+		while(positionNumber < (position - 1)) {
+			// Go to the next element
+			temp = temp -> link();
+			// Increase the position
+			positionNumber++;
 		}
+
+		// Insert the element at the desired position
 		list_insert(temp, entry);
 	}
 
     node::value_type list_remove_at(node*& head_ptr, size_t position) {
-		assert(position >0);
-		assert(position <= list_length(head_ptr));
+		// First, we need to check the preconditions
+		assert((position > 0) && (position <= list_length(head_ptr)));
 		
+		// Create a variable to hold a copy of the data from the removed node (we'll return this)
 		node::value_type value;
-		if(position == 1){
-			value = head_ptr->data();
+
+		// If the position is one, we just need to remove the head node
+		if(position == 1) {
+			value = head_ptr -> data();
 			list_head_remove(head_ptr);
 			return value;
 		}
 
+		// Create a temporary variable to keep track of the node we're at
 		node* temp = head_ptr;
-		size_t pos_count = 1;
-								//iterate to position
-		while(pos_count < position - 1){
-			temp = temp->link();
-			pos_count++;
+		// Create a temporary variable to keep track of the position we're at
+		size_t positionNumber = 1;
+
+		// Loop through the list to the position
+		while(positionNumber < position - 1) {
+			// Go to the next element
+			temp = temp -> link();
+			// Increase the position
+			positionNumber++;
 		}
-		value = temp->data();
-		list_remove(temp);		
+
+		// Set value to the element we're about to remove
+		value = temp -> data();
+
+		// Remove the value from the list
+		list_remove(temp);
+
+		// Return a copy of the data from the removed node	
 		return value;
 	}
 
     node* list_copy_segment(node* head_ptr, size_t start, size_t finish) {
-		assert(1 <= start);
-		assert(start <= finish);
-		assert(finish <= list_length(head_ptr));
+		// First, we need to check the preconditions
+		assert((1 <= start) && (start <= finish) && (finish <= list_length(head_ptr)));
 
-		node* temp_head = NULL;
-								//traverse to start pos
-		for(int i = 1; i < int(start); i++){
-			head_ptr =head_ptr->link();
+		// Create a temporary list to hold the copy (this will be returned)
+		node* copyList = NULL;
+
+		// Go through the list to the start position
+		for(int i = 1; i < int(start); i++) {
+			head_ptr = head_ptr -> link();
 		}
-		list_head_insert(temp_head,head_ptr->data());
-		//std::cout << list_length(temp_head) << endl;
-		node* temp_trav = temp_head;
-								//copy and traverse to finish
-		for(int i = start + 1; i<=int(finish); i++){
-			head_ptr = head_ptr->link();
-			list_insert(temp_trav, head_ptr->data());
-			temp_trav = temp_trav->link();		
-		} 
-		return temp_head;
+
+		//Insert head_ptr -> data() into copyList
+		list_head_insert(copyList, head_ptr -> data());
+
+		// Create a temporary variable for adding information to the list
+		node* temp = copyList;
+		
+		// Loop through from start to finish
+		for(int i = start + 1; i <= int(finish); i++) {
+			head_ptr = head_ptr -> link();
+			list_insert(temp, head_ptr -> data());
+			temp = temp -> link();		
+		}
+
+		// Return the copy
+		return copyList;
 	}
 
     void list_print (const node* head_ptr) {
-		while(head_ptr != NULL){
-			std::cout << head_ptr->data() ;
+		// Go through the list
+		while(head_ptr != NULL) {
+			// Print out the data at the current position of head_ptr
+			std::cout << head_ptr->data();
+
+			// Move head_ptr to the next position
 			head_ptr = head_ptr->link();
-			if(head_ptr)
+
+			// Print a "," when appropriate
+			if(head_ptr) {
 				std::cout<< ", "; 
+			}
 		}
+
+		// After going through the list, end the line
 		std::cout << std::endl;
 	}
 
     void list_remove_dups(node* head_ptr) {
-		if(head_ptr->link() == NULL)
+		// If the list is empty, we don't need to do anything!
+		if((head_ptr -> link()) == NULL) {
 			return;
-									//traverse and check for multiple occurences. If present remove the head
+		}
 		
-		while(head_ptr != NULL){
-			int occ = (list_occurrences(head_ptr, head_ptr->data()));
-			//std::cout <<"occ " << occ<<std::endl;
-			while (occ >1){
-				node* to_del= list_search(head_ptr->link(), head_ptr->data());
+		// Go through the list
+		while(head_ptr != NULL) {
+			// Figure out how many occurrences of data
+			int numOccurrences = (list_occurrences(head_ptr, head_ptr -> data()));
+
+			// If there are multiple occurrences, there are duplicates that need removed!
+			while (numOccurrences > 1) {
+				// Figure out which object to delete
+				node* objToDelete = list_search(head_ptr -> link(), head_ptr -> data());
 				node* cursor;
-				for(cursor = head_ptr; cursor->link() != to_del; cursor = cursor->link());
+				for(cursor = head_ptr; cursor -> link() != objToDelete; cursor = cursor -> link()) {
 					list_remove(cursor);
-				
-				//std::cout <<  "removed " << list_length(head_ptr) << std::endl;
-				occ--;
+				}
+					
+				// Decrement the number of occurrences
+				numOccurrences--;
 			}
-			head_ptr = head_ptr->link();			
+
+			head_ptr = head_ptr -> link();			
 		}
 	}
 
     node* list_detect_loop (node* head_ptr) {
+		// Use Floyd's Cycle Detection Algorithm from class!
+
+		// We need a slow & fast variable to keep track of
 		node* slow = head_ptr;
 		node* fast = head_ptr;
-									//go through and if slow and fast meet there is a loop		
-		while(fast != NULL && fast->link()->link() != NULL){
-			slow = slow->link();
-			fast = fast->link()->link();
+
+		// Loop through the list. Slow and Fast will meet where there's a loop!
+		while((fast != NULL) && ((fast -> link() -> link()) != NULL)) {
+			// Slow will move one step
+			slow = slow -> link();
+
+			// Fast will move two steps
+			fast = fast -> link() -> link();
 			
+			// Once slow and fast are at the same spot, we know there's a loop, so we can move on!
 			if(slow == fast){
 				break;
 			}
 		}
 		
-		if(fast == NULL || fast->link()->link() == NULL)
+		// If any of these are true, then we can return NULL, as there is no loop in the list!
+		if((fast == NULL) || ((fast->link()->link()) == NULL)) {
 			return NULL;
-
-		slow = head_ptr;					//where slow and fast meet is the start of the loop
-		while(slow != fast){
-			slow = slow->link();
-			fast = fast->link();
 		}
+
+		// Now, we can reset slow to the head
+		slow = head_ptr;
+		
+		// Loop through until slow = fast, as where they meet will be the start of the loop!
+		while(slow != fast) {
+			// Move on to next slow & fast
+			slow = slow -> link();
+			fast = fast -> link();
+		}
+
+		// Return a pointer to the start of the loop
 		return slow;
-	}
-    
+	}   
 }
